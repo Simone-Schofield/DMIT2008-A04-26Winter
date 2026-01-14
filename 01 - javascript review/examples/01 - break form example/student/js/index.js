2d35459460229f47f5194884da0f4cb16c4da1a0
Simone-Schofield <Simonealexysschofield@hotmail.com>
2026-01-14 10:59:20 -0700

    Day 1 - JS review - Example 1
    First Commit.

--- 01 - javascript review/examples/01 - break form example/student/js/index.js
+++ 01 - javascript review/examples/01 - break form example/student/js/index.js
@@ -6,3 +6,33 @@ HTML for list topic list item
     NEW TOPIC HERE
 </li>
 */
+
+// 1. select topic & new topic form
+let topicsList = document.querySelector(".topics-list");
+let newTopicForm = document.querySelector(".new-topic-form");
+
+// 2. Add an event listener and stop the event from propgating.
+newTopicForm.addEventListener(
+    "submit",
+    (event) => {
+        event.preventDefault();
+
+        // 3. Get the value of the input from the "form.elements" from the event handler callback.
+        let topicInput = event.target.elements["new-topic"];
+        let newTopic = topicInput.value;
+        // console.log(newTopic);
+
+        // 4. Validate the inputs to make sure the topic is not empty.
+        if (newTopic === "") {
+            topicInput.classList.add("is-invalid");
+        } else {
+            topicInput.classList.remove("is-invalid");
+        }
+
+        // 5. Create a function called "addTopicToPage" that takes the topic name and the topic list element as a parameter.
+
+        // 6. In the "addTopicToPage" function use a template string to create a new list item and add it to the page.
+
+        // 7. Add some topics and test your application!
+    }
+);
