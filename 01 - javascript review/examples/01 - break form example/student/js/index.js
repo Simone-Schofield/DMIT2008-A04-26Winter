36e2a7d2e0c3d0bfa335d682bbffa3add456c14c
Simone-Schofield <Simonealexysschofield@hotmail.com>
2026-01-14 11:34:14 -0700

    Day 1 - JS review - Example 1 (Finished)
    Added newTopicForm.reset(), so it clears the input.

--- 01 - javascript review/examples/01 - break form example/student/js/index.js
+++ 01 - javascript review/examples/01 - break form example/student/js/index.js
@@ -11,6 +11,19 @@ HTML for list topic list item
 let topicsList = document.querySelector(".topics-list");
 let newTopicForm = document.querySelector(".new-topic-form");
 
+// 5. Create a function called "addTopicToPage" that takes the topic name and the topic list element as a parameter.
+const addTopicToPage = (topicName, topicListElement) => {
+    
+    // 6. In the "addTopicToPage" function use a template string to create a new list item and add it to the page.
+    let newTopicElement = `<li class="list-group-item">
+        ${topicName}
+    </li>`;
+
+    topicListElement.innerHTML += newTopicElement;
+    // ^- This is basic a quicker method than topicListElement = topicsList + newTopicElement;
+
+};
+
 // 2. Add an event listener and stop the event from propgating.
 newTopicForm.addEventListener(
     "submit",
@@ -20,7 +33,7 @@ newTopicForm.addEventListener(
         // 3. Get the value of the input from the "form.elements" from the event handler callback.
         let topicInput = event.target.elements["new-topic"];
         let newTopic = topicInput.value;
-        // console.log(newTopic);
+        console.log(newTopic);
 
         // 4. Validate the inputs to make sure the topic is not empty.
         if (newTopic === "") {
@@ -29,10 +42,10 @@ newTopicForm.addEventListener(
             topicInput.classList.remove("is-invalid");
         }
 
-        // 5. Create a function called "addTopicToPage" that takes the topic name and the topic list element as a parameter.
-
-        // 6. In the "addTopicToPage" function use a template string to create a new list item and add it to the page.
-
         // 7. Add some topics and test your application!
+        addTopicToPage(newTopic, topicsList);
+
+        // Clears the input after it's submitted.
+        newTopicForm.reset();
     }
 );
